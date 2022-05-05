93371c47c7ce6625b021609a93b9eda22fc66762
Stanislav <rebel1866@yandex.ru>
2022-05-05 18:50:43 +0300

    security user

--- security/src/main/java/com/epam/esm/security/userdetail/SecurityUser.java
+++ security/src/main/java/com/epam/esm/security/userdetail/SecurityUser.java
@@ -41,33 +41,22 @@ public class SecurityUser implements UserDetails {
     }
 
     @Override
-    public boolean isAccountNonExpired() {
-        return isActive;
-    }
-
-    @Override
     public boolean isAccountNonLocked() {
         return isActive;
     }
 
     @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
     public boolean isCredentialsNonExpired() {
-        return isActive;
+        return true;
     }
 
     @Override
     public boolean isEnabled() {
-        return isActive;
-    }
-
-    public static UserDetails fromUser(User user) {
-        return new org.springframework.security.core.userdetails.User(
-                user.getEmail(), user.getPassword(),
-                user.getStatus().equals(Status.ACTIVE),
-                user.getStatus().equals(Status.ACTIVE),
-                user.getStatus().equals(Status.ACTIVE),
-                user.getStatus().equals(Status.ACTIVE),
-                user.getRole().getAuthorities()
-        );
+        return true;
     }
 }
--- security/src/main/java/com/epam/esm/security/userdetail/UserDetailsServiceImpl.java
+++ security/src/main/java/com/epam/esm/security/userdetail/UserDetailsServiceImpl.java
@@ -1,12 +1,15 @@
 package com.epam.esm.security.userdetail;
 
 import com.epam.esm.dao.UserDao;
+import com.epam.esm.entity.Status;
 import com.epam.esm.entity.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
 
 @Service("userDetailsServiceImpl")
 public class UserDetailsServiceImpl implements UserDetailsService {
@@ -24,6 +27,7 @@ public class UserDetailsServiceImpl implements UserDetailsService {
         if (user == null) {
             throw new UsernameNotFoundException("User doesn't exists");
         }
-        return SecurityUser.fromUser(user);
+        return new SecurityUser(user.getEmail(), user.getPassword(), new ArrayList<>(user.getRole().getAuthorities()),
+                user.getStatus().equals(Status.ACTIVE));
     }
 }
